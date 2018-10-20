d63774250461d5ef8fc8d1a9cbdd7e00a5bb049a
flotisable <s09930698@gmail.com>
2018-10-20 23:06:04 +0800

    basic implementation

--- src/lcs.cpp
+++ src/lcs.cpp
@@ -1,5 +1,125 @@
 #include "lcs.h"
 
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// public member functions
 void LCS::exec( const std::string &a, const std::string &b )
 {
+  mStringA = a;
+  mStringB = b;
+
+  initDatabase  ();
+  evalDatabase  ();
+  collectResults();
+
+  sort( mResults.begin(), mResults.end() );
+
+  printResults();
 }
+// end public member functions
+
+// private member functions
+void LCS::initDatabase()
+{
+  mDatabase.clear();
+
+  mDatabase.resize( mStringA.size() + 1 );
+
+  for( size_t i = 0 ; i < mDatabase.size() ; ++i )
+  {
+     mDatabase[i].resize( mStringB.size() + 1 );
+
+     for( size_t j = 0 ; j < mDatabase[i].size() ; ++j )
+     {
+        if( j == 0 || i == 0 )
+        {
+          mDatabase[i][j].length = 0;
+          mDatabase[i][j].sources.push_back( Source::null );
+        }
+     }
+  }
+}
+
+void LCS::evalDatabase()
+{
+  for( size_t i = 1 ; i < mDatabase.size() ; ++i )
+     for( size_t j = 1 ; j < mDatabase[i].size() ; ++j )
+     {
+        Data &data = mDatabase[i][j];
+
+        // remove A[i] and B[j]
+        data.length = mDatabase[i-1][j-1].length;
+        data.sources.push_back( Source::dec_ab );
+        // end remove A[i] and B[j]
+
+        if( mStringA[i] == mStringB[j] ) ++data.length;
+
+        // A[i] is not in lcs
+        if( mDatabase[i-1][j].length > data.length )
+        {
+          data.length = mDatabase[i-1][j].length;
+          data.sources.clear();
+          data.sources.push_back( Source::dec_a );
+        }
+        else if( mDatabase[i-1][j].length == data.length )
+        {
+          data.sources.push_back( Source::dec_a );
+        }
+        // end A[i] is not in lcs
+
+        // B[j] is not in lcs
+        if( mDatabase[i][j-1].length > data.length )
+        {
+          data.length = mDatabase[i][j-1].length;
+          data.sources.clear();
+          data.sources.push_back( Source::dec_b );
+        }
+        else if( mDatabase[i][j-1].length == data.length )
+        {
+          data.sources.push_back( Source::dec_b );
+        }
+        // end B[j] is not in lcs
+     }
+}
+
+void LCS::collectResults()
+{
+  string lcs;
+
+  collectResults( mStringA.size(), mStringB.size(), lcs );
+}
+
+void LCS::collectResults( size_t i, size_t j, std::string &lcs )
+{
+  for( Source source : mDatabase[i][j].sources )
+  {
+     switch( source )
+     {
+       case Source::dec_a: collectResults( i - 1, j, lcs ); break;
+       case Source::dec_b: collectResults( i, j - 1, lcs ); break;
+       case Source::dec_ab:
+
+         lcs.push_back( mStringA[i] );
+         collectResults( i - 1, j - 1, lcs );
+         lcs.pop_back();
+         break;
+
+       case Source::null:
+       default:
+
+         mResults.push_back( lcs );
+         break;
+     }
+  }
+}
+
+void LCS::printResults()
+{
+  cout << mResults.front().size() << " " << mResults.size() << "\n";
+
+  for( string &lcs : mResults )
+     cout << lcs << "\n";
+}
+// end private member functions
--- src/lcs.h
+++ src/lcs.h
@@ -2,15 +2,43 @@
 #define LCS_H
 
 #include <string>
+#include <vector>
 
 class LCS
 {
   public:
 
+    enum Source
+    {
+      dec_a,
+      dec_b,
+      dec_ab,
+      null
+    };
+
+    struct Data;
+
     void exec( const std::string &a, const std::string &b );
 
   private:
 
+    void initDatabase   ();
+    void evalDatabase   ();
+    void collectResults ();
+    void collectResults ( size_t i, size_t j, std::string &lcs );
+    void printResults   ();
+
+    std::string mStringA;
+    std::string mStringB;
+
+    std::vector<std::vector<Data>>  mDatabase;
+    std::vector<std::string>        mResults;
+};
+
+struct LCS::Data
+{
+  std::vector<Source> sources;
+  int                 length;
 };
 
 #endif
